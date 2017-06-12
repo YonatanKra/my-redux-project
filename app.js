f6075ec190fdac6dc3a5cd4fd305dd3077e90e7e
Yonatan <kra.yonatan@gmail.com>
2017-06-12 23:04:27 +0300

    Added select action
    Extended the state to be an object
    Added select action

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
 function createStore(reducer, initState){
 
     // this is our store's state
-    var list = initState || []; //list of strings
+    var list = initState || {
+            selected: undefined, // the selected index
+            list: [] //list of strings
+        };
     var events = [];
 
     return {
@@ -40,16 +43,22 @@ function listReducer(lastState, action){
     var newState = JSON.parse(JSON.stringify(lastState)); // simple naive clone... better to use lodash :)
     switch(action.type){
         case "ADD_ITEM":
-            newState.push(action.payload);
+            newState.list.push(action.payload);
             break;
         case "DELETE_ITEM":
-            newState.splice(newState.indexOf(action.payload), 1);
+            newState.list.splice(newState.indexOf(action.payload), 1);
             break;
         case "DELETE_ITEM_BY_INDEX":
-            newState.splice(action.payload, 1);
+            newState.list.splice(action.payload, 1);
+            break;
+        case "SELECT":
+            newState.selected = action.payload;
             break;
         case "RESET":
-            return [];
+            return {
+                selected: undefined,
+                list: []
+            };
         default:
             return lastState;
     }
@@ -62,10 +71,19 @@ function listViewHandler(state) {
         return;
     }
     var listHtml = '';
-    for (var i = 0; i < state.length; i++) {
-        listHtml += '<li onclick="selectItem()" data-index="' + i + '" class="list-item">' + state[i] + '</li>';
+    for (var i = 0; i < state.list.length; i++) {
+        listHtml += '<li onclick="selectItem()" data-index="' + i + '" class="list-item">' + state.list[i] + '</li>';
     }
     $('#list').html(listHtml);
+    
+    $($('#list').children()[state.selected]).addClass('selected');
+}
+
+function selectItem(){
+    myStore.dispatch({
+        type: "SELECT",
+        payload: event.target.dataset.index
+    });
 }
 
 function addItemButton() {
@@ -80,7 +98,17 @@ function addItemButton() {
     });
 }
 
-var INIT_STATE = ['Yuval', 'Arbel'];
+function deletItem(){
+    myStore.dispatch({
+        type: "DELETE_ITEM_BY_INDEX",
+        payload: myStore.getState().selected
+    });
+}
+
+var INIT_STATE = {
+    selected: 1,
+    list: ['Yuval', 'Arbel']
+};
 
 var myStore = createStore(listReducer, INIT_STATE);
 
